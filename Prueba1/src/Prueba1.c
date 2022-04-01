75145a36545d429200fd35f91960b7b29712f537
Ivan Kersevan <ivankersevan13@hotmail.com.ar>
2022-04-01 00:30:41 -0300

    a ver si funciona

--- Prueba1/src/Prueba1.c
+++ Prueba1/src/Prueba1.c
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	printf("Hola Ivan, esto funciona de maravilla y espectacular");
+	printf("Hola Ivan, esto funciona de maravilla y super bien");
 	return 0;
 }
